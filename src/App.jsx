c5be31630ca16821746f45890b520baec7ed5af7
Nick Tse <nicholashtse@gmail.com>
2023-06-27 00:58:39 -0400

    add services section outline; WIP

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import AboutUs from './components/AboutUs'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Services from './components/Services'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Hero />
       <AboutUs />
+      <Services />
     </>
   )
 }
---
+++ src/components/Services.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./css/Services.css";
+import { FaCarSide, FaDog } from 'react-icons/fa';
+import { IoFootstepsSharp } from 'react-icons/io5';
+import { GiComb } from 'react-icons/gi';
+import { LuHotel } from 'react-icons/lu';
+import { BiSolidBone } from 'react-icons/bi';
+import { MdHealthAndSafety, MdSports } from 'react-icons/md';
+
+const Services = () => {
+  return (
+    <div className="container">
+      <div className="services-wrapper">
+        <div className="services-content">
+          <h2 className="headline">Services</h2>
+          <div className="services-graphic">
+            <div className="service">
+              <h4>Dog Walking</h4>
+              <IoFootstepsSharp className="service-icon"/>
+            </div>
+            <div className="service">
+              <h4>Pet Sitting</h4>
+              <FaDog className="service-icon"/>
+            </div>
+            <div className="service">
+              <h4>Grooming</h4>
+              <GiComb className="service-icon"/>
+            </div>
+            <div className="service">
+              <h4>Boarding & Daycare</h4>
+              <LuHotel className="service-icon"/>
+            </div>
+            <div className="service">
+              <h4>Training</h4>
+              <MdSports className="service-icon"/>
+            </div>
+            <div className="service">
+              <h4>Tranportation</h4>
+              <FaCarSide className="service-icon"/>
+            </div>
+            <div className="service">
+              <h4>Supply Delivery</h4>
+              <BiSolidBone className="service-icon"/>
+            </div>
+            <div className="service">
+              <h4>Health Checkup</h4>
+              <MdHealthAndSafety className="service-icon"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
